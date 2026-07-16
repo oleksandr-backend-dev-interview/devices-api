db8d80aa4c23538fe4f4a630b8460c2d60d02357
Oleksandr Poslavskyi interview <poslavskij92@gmail.com>
2026-07-16 17:27:48 +0200

    chore: initialize Java 21 Spring Boot project

---
+++ src/test/java/dev/poslavskyi/devices/DevicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.poslavskyi.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
