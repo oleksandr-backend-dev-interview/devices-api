7fd737eb19f5baaae8e32ff6632db833f31b2767
Oleksandr Poslavskyi interview <poslavskij92@gmail.com>
2026-07-19 13:45:49 +0200

    fix: add Testcontainers to DevicesApiApplicationTests. It didn't fail before this fix because compose Postgres happened to be running and datasource at localhost:5432 was available

--- src/test/java/dev/poslavskyi/devices/DevicesApiApplicationTests.java
+++ src/test/java/dev/poslavskyi/devices/DevicesApiApplicationTests.java
@@ -2,12 +2,21 @@ package dev.poslavskyi.devices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class DevicesApiApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static final PostgreSQLContainer POSTGRES =
+			new PostgreSQLContainer("postgres:17-alpine");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
